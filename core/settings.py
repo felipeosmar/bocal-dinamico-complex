4b5abdd6e0d37960ac696f6ee39623d96ad3a98e
Felipe O. de Aviz <felipe.aviz@sc.senai.br>
2025-12-12 17:14:02 -0300

    ajuste pasta static

--- core/settings.py
+++ core/settings.py
@@ -114,9 +114,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'apps' / 'web' / 'static',
-]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+_static_app_dir = BASE_DIR / 'apps' / 'web' / 'static'
+if _static_app_dir.exists():
+    STATICFILES_DIRS = [
+        _static_app_dir,
+    ]
+else:
+    STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
